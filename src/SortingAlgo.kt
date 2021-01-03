3ba8a4ecc016b2466808302102701834d5136b5c
Anuj <anujwebhost@gmail.com>
2021-01-03 18:01:20 +0530

    test

--- src/SortingAlgo.kt
+++ src/SortingAlgo.kt
@@ -1,10 +1,31 @@
 
 
 fun main(){
-
-    var arr= arrayOf(43,1,4,46,6562,7,8,0,99,86)
+    var arr= arrayOf(43,1,4,46,0,7,5)
+   /* var arr= arrayOf(43,1,4,46,6562,7,8,0,99,86)*/
    // bubblesort(arr)
  /* selectionshort(arr)*/
+    insertionshort(arr)
+
+}
+
+fun insertionshort(arr: Array<Int>) {
+    for(j in 0 until arr.size){
+        for(i in j downTo  1){
+          /*  print("$i and ${i-1}")*/
+            if(arr[i]<arr[i-1]){
+                var a=arr[i]
+                var b=arr[i-1]
+                arr[i-1]=a
+                arr[i]=b
+            }
+        }
+    }
+
+    arr.forEach {
+        print(it)
+        print(" ")
+    }
 
 }
 
@@ -50,7 +71,7 @@ fun bubblesort(arr: Array<Int>) {
     }
    for(i in arr){
        print(i.toString())
-       print("\n")
+       print(" ")
    }
 
 }
