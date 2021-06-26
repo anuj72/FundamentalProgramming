848ec1aec71320f49330f7745bc741183ced8632
Anuj Sachan <anuj.sachan@mounty.co>
2021-06-26 16:12:46 +0530

    hackerRank Solution Added

--- src/MountyFavourite/MoreThanOneOccorence.kt
+++ src/MountyFavourite/MoreThanOneOccorence.kt
@@ -1,13 +1,35 @@
 package MountyFavourite
 
 fun main() {
+    //1st solution
     var arr = arrayOf("anand", "anuj", "abhinandan", "akhil")
-    var farr=ArrayList<String>()
-    arr.forEach {str1->
-        var len=  str1.count{ c -> c == 'a' }
-        if(len>1){ farr.add(str1) }
+    var farr = ArrayList<String>()
+    arr.forEach { str1 ->
+        var len = str1.count { c -> c == 'a' }
+        if (len > 1) {
+            farr.add(str1)
+        }
     }
     print(farr)
+    //2nd solution
+    var farr1 = ArrayList<String>()
+    arr.forEach { str1 ->
+        var len = str1.windowed(1).filter { it == "a" }.count()
+        if (len > 1) {
+            farr1.add(str1)
+        }
+    }
+    print(farr1)
+
+
+//3rd solution
+    val marr = arr.filter { it.windowed(1).filter { it == "a" }.count()>1}
+    print(marr)
+
+
+    //4th solution
+    val marr1 = arr.filter { it.count { c -> c == 'a' }>1}
+    print(marr1)
 
 
 
